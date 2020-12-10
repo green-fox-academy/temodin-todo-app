85e075cd1ecd9b694538b212f70a11ce2e10b220
Balint Solyom <solyom.balint@gmail.com>
2020-12-10 17:32:47 +0100

    tasks can be added, removed. task status exists and can be printed but cannot be changed yet. error handling for the added functionalities done

--- src/ArgumentHandler.java
+++ src/ArgumentHandler.java
@@ -1,3 +1,4 @@
+import javax.swing.text.DefaultEditorKit;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -10,49 +11,116 @@ import java.util.List;
 public class ArgumentHandler {
     public void argumentHandler(String[] arguments) {
         if (arguments.length == 0) {
-            printManualFileContent(getFileContent(Paths.get("../Data/manual.txt")));
-        }
-        else if (arguments[0].equals("-l")) {
-            printToDoFileContent(getFileContent(Paths.get("../Data/todo.txt")));
-        }
-        else if (arguments[0].equals("-a")) {
-            addNewTask(Paths.get("../Data/todo.txt"),arguments[1]);
+            printManualFileContent();
+        } else if (arguments[0].equals("-l")) {
+            printToDoFileContent(getToDoFileContent(Paths.get("../Data/todo.txt")));
+        } else if (arguments[0].equals("-a")) {
+            try {
+                addNewTask(arguments[1]);
+            } catch (ArrayIndexOutOfBoundsException e) {
+                System.out.println("Unable to add: no task provided");
+            }
+
+        } else if (arguments[0].equals("-r")) {
+            if (arguments.length < 2) {
+                System.out.println("Unable to remove: no index provided");
+            } else removeTask(arguments[1]);
+        } else {
+            System.out.println("Unsupported argument(s)");
+            printManualFileContent();
         }
 
     }
 
-    public List<String> getFileContent(Path filePath) {
-        Path pathManual = filePath;
-        List<String> fileContent = new ArrayList<>();
+    public List<String[]> getToDoFileContent(Path filePath) {
+        List<String> fileContentLines = new ArrayList<>();
         try {
-            fileContent = Files.readAllLines(filePath);
+            fileContentLines = Files.readAllLines(filePath);
         } catch (IOException e) {
             System.out.println("File not found.");
         }
+        List<String[]> fileContent = new ArrayList<>();
+        for (String line : fileContentLines
+        ) {
+            fileContent.add(line.split(","));
+        }
         return fileContent;
-
     }
 
-    public void printManualFileContent(List<String> fileContent) {
-        for (String line : fileContent
+    public void printManualFileContent() {
+        List<String> manualLines = new ArrayList<>();
+        Path manualPath = Paths.get("../Data/manual.txt");
+        try {
+            manualLines = Files.readAllLines(manualPath);
+        } catch (IOException e) {
+            System.out.println("Manual not found.");
+            return;
+        }
+
+        for (String line : manualLines
         ) {
             System.out.println(line);
         }
     }
-    public void printToDoFileContent(List<String> fileContent) {
+
+    public void printToDoFileContent(List<String[]> fileContent) {
         if (fileContent.size() == 0) {
             System.out.println("No todos for today! :)");
-        }
-        for (int i = 0; i < fileContent.size(); i++) {
-            System.out.println(i+1 + " - " + fileContent.get(i));
+        } else {
+            for (int i = 0; i < fileContent.size(); i++) {
+                System.out.print(i + 1 + " - " + statusPrint(fileContent.get(i)[0]) + " " + fileContent.get(i)[1]);
+                System.out.println();
+            }
         }
     }
 
-    private void addNewTask (Path filePath, String task) {
+    private String statusPrint(String statusFlag) {
+        if (statusFlag.equals("1")) {
+            return "[x]";
+        } else return "[ ]";
+    }
+
+    private void addToFile(Path filePath, String task) {
         try {
-            Files.write(filePath, Collections.singleton("/n" + task), StandardOpenOption.APPEND);
+            Files.write(filePath, Collections.singleton("0," +task), StandardOpenOption.APPEND);
         } catch (IOException e) {
             System.out.println("Cannot write file");
         }
     }
-}
+
+    private void addNewTask(String task) {
+        addToFile(Paths.get("../Data/todo.txt"), task);
+    }
+
+    private void removeTask(String input) {
+        Path toDoPath = Paths.get("../Data/todo.txt");
+        Integer index;
+        try {
+            index = Integer.parseInt(input);
+        } catch (NumberFormatException e) {
+            System.out.println("Second argument must be a number");
+            return;
+        }
+        List<String[]> fileContent = getToDoFileContent(toDoPath);
+        try {
+            fileContent.remove(index - 1);
+        } catch (IndexOutOfBoundsException e) {
+            System.out.println("Index is out of bound");
+        }
+        try {
+            Files.write(toDoPath, concatenator(fileContent));
+        } catch (IOException e) {
+            System.out.println("Cannot write file");
+        }
+
+    }
+
+    private List<String> concatenator (List<String[]> listToConvert) {
+        List<String> listToReturn = new ArrayList<>();
+        for (int i = 0; i < listToConvert.size(); i++) {
+            listToReturn.add(listToConvert.get(i)[0] + "," + listToConvert.get(i)[1]);
+        }
+        return listToReturn;
+        }
+    }
+
